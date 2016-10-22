873912786621f634734a79b170910a37621ab890
hxndgtlmg <hxndgtlmg@users.noreply.github.com>
2016-10-23 00:00:47 +0300

    Переписан под PEP8

--- pprint_json.py
+++ pprint_json.py
@@ -6,8 +6,10 @@ def load_data(filepath):
         data = json.loads(file.read())
     return data
 
+
 def pretty_print_json(data):
     return json.dumps(data, sort_keys=True, indent=4)
+
 
 if __name__ == '__main__':
     data = load_data(input("Введите путь к файлу: "))
