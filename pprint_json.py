0d35872da94eb3c8e31a57b0980cbefaa32f856a
hxndgtlmg <hxndgtlmg@users.noreply.github.com>
2016-10-23 02:07:17 +0300

    Скорректированы названия переменных

--- pprint_json.py
+++ pprint_json.py
@@ -7,10 +7,10 @@ def load_data(filepath):
     return data
 
 
-def pretty_print_json(data):
-    return json.dumps(data, sort_keys=True, indent=4)
+def pretty_print_json(json_data):
+    return json.dumps(json_data, sort_keys=True, indent=4)
 
 
 if __name__ == '__main__':
-    data = load_data(input("Введите путь к файлу: "))
-    print(pretty_print_json(data))
+    json_data = load_data(input("Введите путь к файлу: "))
+    print(pretty_print_json(json_data))
